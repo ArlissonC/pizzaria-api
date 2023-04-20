c28590f6fe31a40e793603219659dee6e69b30ec
Arlisson Costa <arlisson.fs13@gmail.com>
2023-04-19 23:57:58 -0300

    configuring postgres and creating models

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/teste", (req, res) => {
-  return res.json({ ok: true });
+  throw new Error("Erro ao fazer essa requisição");
 });
 
 export { router };
