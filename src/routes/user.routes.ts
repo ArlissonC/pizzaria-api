84beb4f85b95c3bf37340b6dd54888d80e4c167d
Arlisson Costa <arlisson.fs13@gmail.com>
2023-04-22 14:45:01 -0300

    user details function

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/user/CreateUserController";
 import { AuthUserController } from "../controllers/user/AuthUserController";
+import { DetailUserController } from "../controllers/user/DetailUserController";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
 
 const userRoutes = Router();
 
 userRoutes.post("/", new CreateUserController().handle);
 userRoutes.post("/session", new AuthUserController().handle);
+userRoutes.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { userRoutes };
