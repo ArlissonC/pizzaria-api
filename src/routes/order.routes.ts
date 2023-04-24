db794289dd5089f062051791d42b0ceacf47a3d2
Arlisson Costa <arlisson.fs13@gmail.com>
2023-04-24 14:59:12 -0300

    add items to order

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
 import { CreateOrderController } from "../controllers/order/CreateOrderController";
 import { RemoverOrderController } from "../controllers/order/RemoverOrderController";
+import { AddItemController } from "../controllers/order/AddItemController";
 
 const orderRoutes = Router();
 
 orderRoutes.post("/", isAuthenticated, new CreateOrderController().handle);
 orderRoutes.delete("/", isAuthenticated, new RemoverOrderController().handle);
+orderRoutes.post("/add", isAuthenticated, new AddItemController().handle);
 
 export { orderRoutes };
