2d8e703a1e9123159ba2da223814072de8da40ec
Arlisson Costa <arlisson.fs13@gmail.com>
2023-04-25 19:20:36 -0300

    changing draft value

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -4,12 +4,18 @@ import { CreateOrderController } from "../controllers/order/CreateOrderControlle
 import { RemoverOrderController } from "../controllers/order/RemoverOrderController";
 import { AddItemController } from "../controllers/order/AddItemController";
 import { RemoveItemController } from "../controllers/order/RemoveItemController";
+import { SendOrderController } from "../controllers/order/SendOrderController";
 
 const orderRoutes = Router();
 
 orderRoutes.post("/", isAuthenticated, new CreateOrderController().handle);
 orderRoutes.delete("/", isAuthenticated, new RemoverOrderController().handle);
 orderRoutes.post("/add", isAuthenticated, new AddItemController().handle);
-orderRoutes.post("/remove", isAuthenticated, new RemoveItemController().handle);
+orderRoutes.delete(
+  "/remove",
+  isAuthenticated,
+  new RemoveItemController().handle,
+);
+orderRoutes.put("/send", isAuthenticated, new SendOrderController().handle);
 
 export { orderRoutes };
