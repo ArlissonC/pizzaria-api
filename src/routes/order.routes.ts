9e045e578761e86d69b42e10202aad956b71a429
Arlisson Costa <arlisson.fs13@gmail.com>
2023-04-25 19:05:05 -0300

    removing items from table

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -3,11 +3,13 @@ import { isAuthenticated } from "../middlewares/isAuthenticated";
 import { CreateOrderController } from "../controllers/order/CreateOrderController";
 import { RemoverOrderController } from "../controllers/order/RemoverOrderController";
 import { AddItemController } from "../controllers/order/AddItemController";
+import { RemoveItemController } from "../controllers/order/RemoveItemController";
 
 const orderRoutes = Router();
 
 orderRoutes.post("/", isAuthenticated, new CreateOrderController().handle);
 orderRoutes.delete("/", isAuthenticated, new RemoverOrderController().handle);
 orderRoutes.post("/add", isAuthenticated, new AddItemController().handle);
+orderRoutes.post("/remove", isAuthenticated, new RemoveItemController().handle);
 
 export { orderRoutes };
